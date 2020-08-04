5478c540c75c99b81689faa77e981469014db751
F8837059 Rubens de Melo Neto <rubensdemelo@bb.com.br>
2020-08-04 11:51:58 -0300

    dark

--- src/js/components/theme-toggle.js
+++ src/js/components/theme-toggle.js
@@ -58,9 +58,7 @@ class ThemeToggle extends HTMLElement {
   }
 
   setButtonLabelAndStatus(currentSetting) {
-    this.modeToggleButton.innerText = `${
-      currentSetting === 'dark' ? 'Light' : 'Dark'
-    } theme`;
+    this.modeToggleButton.innerText = `${currentSetting === 'dark' ? 'Light' : 'Dark'}`;
     this.modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
   }
 
@@ -69,7 +67,7 @@ class ThemeToggle extends HTMLElement {
       <div class="[ theme-toggle ] [ md:ta-right gap-top-500 ]">
         <div role="status" class="[ visually-hidden ][ js-mode-status ]"></div>
         <button class="[ button ] [ font-base text-base weight-bold ] [ js-mode-toggle ]">
-          Dark theme
+          Dark
         </button>
       </div>
     `;
