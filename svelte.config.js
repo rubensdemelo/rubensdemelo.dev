0285a873467429b3501c4d4c84de12c6390e9412
rubensdemelo <rubensdemelo@gmail.com>
2021-05-28 14:19:42 -0300

    ajuste svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
-import adapterStatic from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,13 +14,7 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		// adapter: adapterStatic(),
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
